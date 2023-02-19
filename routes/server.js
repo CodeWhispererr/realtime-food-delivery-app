c4f4bdf9693caa8e17a6c8b6b79903defb6c8c4b
Harshit Dubey <80579894+Harshit905@users.noreply.github.com>
2023-02-20 04:00:52 +0530

    initial

---
+++ routes/server.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+// Dynamic Port
+const PORT=process.env.PORT||3000
+
+//Template Engine
+const ejs=require("ejs");
+
+//Express layout
+const expressLayout=require('express-ejs-layouts');
+
+// set template engine
+app.set("views", path.join(__dirname,"../resources/views"));
+app.set("view engine","ejs");
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+    // res.send("This is a Home Page");
+    // console.log("Home Route");
+});
+
+app.use(expressLayout);
+
+
+app.listen( PORT,() => {
+    console.log(`Expresso ☕ is on Port ${ PORT } Ctrl + C to Stop `) 
+});
+
