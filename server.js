5defca240c31f4f6418fdf3959254cbedee7ef21
Harshit Dubey <80579894+Harshit905@users.noreply.github.com>
2023-03-25 11:58:47 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+require("dotenv").config();
 const express=require("express");
 const app=express();
 const path=require("path");
+const session=require('express-session');
+const flash=require("express-flash");
+const MongoDbStore=require("connect-mongo");
+
 // Dynamic Port
 const PORT=process.env.PORT||3000
+const mongoose=require('mongoose');
+mongoose.set('strictQuery', true);
+//Connect mongoDB
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log("connection successful...")).catch((err)=>console.log(err));
+const connection = mongoose.connection; 
+// connection.once('open', () => {
+// console.log('Database connected...');
+// }).catch(err,() => {
+// console.log('Connection failed...')
+// });
 
 //Template Engine
 const ejs=require("ejs");
@@ -11,10 +27,38 @@ const ejs=require("ejs");
 const expressLayout=require('express-ejs-layouts');
 
 
+//session store
+// let mongoStore=new MongoDbStore({
+//     mongooseConnection:connection,
+//     collection:'sessions'
+// })
+
+// session config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+resave: false,
+store:MongoDbStore.create({
+    mongoUrl:url
+}),
+// strore:mongoStore,
+saveUninitialized: false,
+// cookie: { maxAge: 1000*15 }
+cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}))
+
+
+
+app.use(flash());
 
 //Assets 
 app.use(express.static('public'));
+app.use(express.json());
 
+//global middlewares
+app.use((req,res,next)=>{
+res.locals.session=req.session
+next()
+})
 
 // set template engine
 app.use(expressLayout);
@@ -22,21 +66,8 @@ app.set("views", path.join(__dirname,"/resources/views"));
 app.set("view engine","ejs");
 
 
-
-app.get("/",(req,res)=>{
-    res.render("home");
-    // res.send("This is a Home Page");
-});
-
-app.get("/cart",(req,res)=>{
-    res.render("customers/cart");
-});
-app.get("/login",(req,res)=>{
-    res.render("auth/login");
-});
-app.get("/register",(req,res)=>{
-    res.render("auth/register");
-});
+const initRoutes=require("./routes/web")
+initRoutes(app);
 
 app.listen( PORT,() => {
     console.log(`Expresso ☕ is on Port ${ PORT } Ctrl + C to Stop `) 
